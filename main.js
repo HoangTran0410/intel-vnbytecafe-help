5ecda7d31234af1ecf4b4dabb19f52e55504460b
Hoang <th01634044850@gmail.com>
2022-01-25 14:24:04 +0700

    [Refactor] using href instead of collapsible content

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 window.onload = () => {
   //#region collapsible: https://www.w3schools.com/howto/howto_js_collapsible.asp
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
+  // var coll = document.getElementsByClassName("collapsible");
+  // var i;
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  }
+  // for (i = 0; i < coll.length; i++) {
+  //   coll[i].addEventListener("click", function () {
+  //     this.classList.toggle("active");
+  //     var content = this.nextElementSibling;
+  //     if (content.style.maxHeight) {
+  //       content.style.maxHeight = null;
+  //     } else {
+  //       content.style.maxHeight = content.scrollHeight + "px";
+  //     }
+  //   });
+  // }
   //#endregion
 
   //#region modal image
@@ -60,9 +60,9 @@ window.onload = () => {
 
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
-    // document.body.scrollTop = 0; // For Safari
-    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    // window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   scrollToTopButton.onclick = topFunction;
